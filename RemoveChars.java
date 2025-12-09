d3a15c61d2200f8ac082f608ad651b012293fa4c
Varun Batta <rajnibatta321@gmail.com>
2025-12-09 16:54:29 +0530

    Day 4

---
+++ RemoveChars.java
@@ -0,0 +1,13 @@
+public class RemoveChars {
+    public static String remove(String str) {
+      int dist = str.length();
+      String Sample = str.substring(1,(dist-1));
+      return Sample;
+      
+      
+      
+      
+
+        
+    }
+}
